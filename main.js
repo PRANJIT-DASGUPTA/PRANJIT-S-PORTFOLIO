fab87aeae6505965b49ccddb114ae3ab5d707f84
PRANJIT DASGUPTA <pranjitdasgupta@gmail.com>
2026-06-17 19:47:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed=new Typed(".text", {
+    strings: ["Frontend Developer" , "App Developer" ,"Artist" ,"Photographer","Chess Player"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 100,
+    loop:true
+});
+function scrolll() {
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(350, 0)
+}
+
+function scrollr() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(-350, 0)
+}
